657c15f4c13f38c232f204920aeb05df63248944
Pauvunraj-Eswaran <pauvunraj2008@gmail.com>
2026-06-02 11:56:18 +0530

    added commands

--- number.py
+++ number.py
@@ -1,8 +1,10 @@
 import random
 num=random.randint(1, 100)
 print("Welcome to the random number game!")
+count=0
 while True:
     guess = int(input("Guess a number between 1 and 100: "))
+    count += 1
     if guess == num:
         print("Congratulations! You guessed the number!")
         break
@@ -10,3 +12,4 @@ while True:
         print("Too low! Try again.")
     else:
         print("Too high! Try again.")
+print("You guessed the number in", count, "tries.")
